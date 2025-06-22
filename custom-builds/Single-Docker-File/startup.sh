ecdddf6ca427472020addb7f0324e948d8f7ae8a
Kelvin Appiah <102846107+KelpyShades@users.noreply.github.com>
2025-06-22 11:04:23 +0000

    Comment out database initialization and supervisord configuration for compreface-postgres-db in startup.sh and supervisord.conf

--- custom-builds/Single-Docker-File/startup.sh
+++ custom-builds/Single-Docker-File/startup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # EXTERNAL_DB defines if we need to run internal DB
-external_db=${EXTERNAL_DB:-false}
-if [ "$external_db" = false ] ; then
-    # restore default data if it was cleared by volume creation
-    if [ -z "$(ls -A $PGDATA)" ]; then
-       echo "Postgres directory is empty. Copy default values into it"
-       cp -r /var/lib/postgresql/default/* $PGDATA
-    fi
-    # change permissions in case they were corrupted
-    chown -R postgres:postgres $PGDATA
-    chmod 700 $PGDATA
+# external_db=${EXTERNAL_DB:-false}
+# if [ "$external_db" = false ] ; then
+#     # restore default data if it was cleared by volume creation
+#     if [ -z "$(ls -A $PGDATA)" ]; then
+#        echo "Postgres directory is empty. Copy default values into it"
+#        cp -r /var/lib/postgresql/default/* $PGDATA
+#     fi
+#     # change permissions in case they were corrupted
+#     chown -R postgres:postgres $PGDATA
+#     chmod 700 $PGDATA
 
-    echo Starting compreface-postgres-db
-    supervisorctl start compreface-postgres-db
-fi
+#     echo Starting compreface-postgres-db
+#     supervisorctl start compreface-postgres-db
+# fi
 
 # wait until DB starts
 sleep 10
